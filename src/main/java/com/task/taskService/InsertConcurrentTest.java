99a3a413d8301d89c409b247593e5ff39f8524b1
menglinghui16 <1551799812@qq.com>
2019-06-05 21:50:10 +0800

    Initial commit

---
+++ src/main/java/com/task/taskService/InsertConcurrentTest.java
@@ -0,0 +1,11 @@
+package com.task.taskService;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2019/6/5.
+ */
+@Component
+public class InsertConcurrentTest {
+
+}
